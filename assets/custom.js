fdc667e981185d96e0a048bad840ea62bd53601c
AKJPLAY <38575828+AKJPLAY@users.noreply.github.com>
2022-09-30 16:39:39 +0530

    update anncoument bar

--- assets/custom.js
+++ assets/custom.js
@@ -1,10 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+
+if (!customElements.get('announcement-bar')) {
+  customElements.define('announcement-bar', class AnnoucmentBar extends HTMLElement {
+    constructor() {
+      super();
+
+      this.swiperContainer = this.querySelector('.swiper');
+      const swiper = new Swiper(this.swiperContainer, {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      });
     }
-  });
+  })
+}
+
+  
